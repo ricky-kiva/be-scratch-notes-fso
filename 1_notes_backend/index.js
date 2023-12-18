7427e78ef5e154163a993d168b2cdc9ce52171b4
ricky-kiva <ricky.kiva@gmail.com>
2023-12-18 11:41:40 +0700

    feat(index): impl. `Express` to server

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -18,11 +19,15 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
