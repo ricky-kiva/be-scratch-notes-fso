a724cc5ad254304e1ec7ac6fd730116445c7a1aa
ricky-kiva <ricky.kiva@gmail.com>
2023-12-18 11:24:02 +0700

    build(npm): add Express depend.

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -1,8 +1,26 @@
 const http = require('http')
 
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
 const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('Hello World')
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(notes))
 })
 
 const PORT = 3001
