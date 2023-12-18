5250b5b5b5882e6fe246921ccf706c1bb0b1455c
ricky-kiva <ricky.kiva@gmail.com>
2023-12-18 12:05:32 +0700

    feat(index): add route to single `Note` resource

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -27,6 +27,12 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    response.json(note)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
