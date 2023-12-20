0dee94ffa331c52fd6502b39db2b7a2f499147e6
ricky-kiva <ricky.kiva@gmail.com>
2023-12-20 12:02:30 +0700

    fix(index): fix var. typo on deleting

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -41,7 +41,7 @@ app.get('/api/notes/:id', (request, response) => {
 
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    note = notes.filter(note => note.id !== id)
+    notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
 })
