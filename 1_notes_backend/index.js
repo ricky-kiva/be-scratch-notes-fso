69a39f83042e80488372d19cb75699d745dfe7b8
ricky-kiva <ricky.kiva@gmail.com>
2023-12-20 12:35:08 +0700

    feat(index): add Note by Post

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json()) // impl. json-parser on Express
+
 let notes = [
     {
         id: 1,
@@ -39,12 +41,36 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+    
+    notes = notes.concat(note)
+    response.json(note)
+})
+
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
 })
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
