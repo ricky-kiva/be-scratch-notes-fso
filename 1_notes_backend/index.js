7e2224ec9e4025341b484b5ca5177bc6737e32ab
ricky-kiva <ricky.kiva@gmail.com>
2023-12-23 08:26:53 +0700

    feat(index): adj.Port for deployment

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -102,7 +102,7 @@ const generateId = () => {
 
 app.use(unknownEndpoint) // impl. custom middleware
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
