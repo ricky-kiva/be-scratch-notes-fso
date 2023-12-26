cb0990076f23444e8729662438eaddc760cc9add
ricky-kiva <ricky.kiva@gmail.com>
2023-12-26 22:54:06 +0700

    refactor(index): remove root GET on BE

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -50,10 +50,6 @@ let notes = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
