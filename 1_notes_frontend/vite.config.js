8d77a53d06a1f0d5ef854c8f466d636d79440dee
ricky-kiva <ricky.kiva@gmail.com>
2023-12-27 13:35:40 +0700

    fix(fe): impl. Proxy on failed access FE dev. mode after refactor relative base URL

--- 1_notes_frontend/vite.config.js
+++ 1_notes_frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': { // only req. made to paths starting with `/api` are redirected to `target (backend)`
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  }
 })
