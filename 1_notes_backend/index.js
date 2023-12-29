75aeb2a96c2ffbc84532759ff600d2ebad52afc7
ricky-kiva <ricky.kiva@gmail.com>
2023-12-29 23:11:58 +0700

    feat(index): impl. error middleware to POST `Note` handler

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -53,7 +53,7 @@ app.get('/api/notes/:id', (request, response, next) => {
         })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (body.content === undefined) {
@@ -68,7 +68,9 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false
     })
 
-    note.save().then(savedNote => { response.json(savedNote) })
+    note.save()
+        .then(savedNote => { response.json(savedNote) })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -106,6 +108,11 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ 
             status: 'error',
             error: 'malformatted id' 
+        })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({
+            status: 'error',
+            error: error.message
         })
     }
 
