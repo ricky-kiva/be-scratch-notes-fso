44f422e6ee9e4cebf9400a99591fe4dc5397b207
ricky-kiva <ricky.kiva@gmail.com>
2023-12-30 21:47:26 +0700

    fix(eslint): fix indent. & unused var.

--- 1_notes_backend/index.js
+++ 1_notes_backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 require('dotenv').config()
 
@@ -9,11 +8,11 @@ const Note = require('./models/note')
 
 const app = express()
 
-morgan.token('post-body', (req, res) => {
-    if (req.method === 'POST' && req.body) {
-        return JSON.stringify(req.body)
-    }
-    return ' '
+morgan.token('post-body', (request, _response) => {
+  if (request.method === 'POST' && request.body) {
+    return JSON.stringify(request.body)
+  }
+  return ' '
 })
 
 app.use(express.json()) // impl. json-parser on Express
@@ -33,95 +32,95 @@ const requestLogger = (request, response, next) => {
 // app.use(requestLogger) // impl. custom middleware
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-    const id = request.params.id
+  const id = request.params.id
 
-    Note.findById(id)
-        .then(note => 
-            note 
-                ? response.json(note) 
-                : response.status(404).end() // handle if no res. with such `id` is found
-        )
-        .catch(error => {
-            // handle if `id` is not in MongoDB `id` format
-            next(error) // `next(...)` will pass execution to `error handler middleware`
-        })
+  Note.findById(id)
+    .then(note => 
+      note 
+        ? response.json(note) 
+        : response.status(404).end() // handle if no res. with such `id` is found
+    )
+    .catch(error => {
+      // handle if `id` is not in MongoDB `id` format
+      next(error) // `next(...)` will pass execution to `error handler middleware`
+    })
 })
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (body.content === undefined) {
-        return response.status(400).json({ 
-            status: 'error',
-            message: 'content missing' 
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false
+  if (body.content === undefined) {
+    return response.status(400).json({ 
+      status: 'error',
+      message: 'content missing' 
     })
+  }
 
-    note.save()
-        .then(savedNote => { response.json(savedNote) })
-        .catch(error => next(error))
+  const note = new Note({
+    content: body.content,
+    important: body.important || false
+  })
+
+  note.save()
+    .then(savedNote => { response.json(savedNote) })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-    const id = request.params.id
+  const id = request.params.id
     
-    Note.findByIdAndDelete(id)
-        .then(result => response.status(204).end())
-        .catch(error => next(error))
+  Note.findByIdAndDelete(id)
+    .then(_result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
-    const id = request.params.id
+  const body = request.body
+  const id = request.params.id
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
+  const note = {
+    content: body.content,
+    important: body.important
+  }
 
-    // `new: true` will return updated doc.
-    // `runValidators: true` will apply validator stated on schema on PUT
-    // `context: query` need to also be set along with `runValidators: true` for some technical reasons
-    Note.findByIdAndUpdate(
-        id, note, { new: true, runValidators: true, context: 'query' }
-    ) 
-        .then(updatedNote => response.json(updatedNote))
-        .catch(error => next(error))
+  // `new: true` will return updated doc.
+  // `runValidators: true` will apply validator stated on schema on PUT
+  // `context: query` need to also be set along with `runValidators: true` for some technical reasons
+  Note.findByIdAndUpdate(
+    id, note, { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedNote => response.json(updatedNote))
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) =>
-    response.status(404).send({
-        status: 'error', 
-        message: 'Unknown Endpoint'
-    })
+  response.status(404).send({
+    status: 'error', 
+    message: 'Unknown Endpoint'
+  })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ 
-            status: 'error',
-            error: 'malformatted id' 
-        })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({
-            status: 'error',
-            error: error.message
-        })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ 
+      status: 'error',
+      error: 'malformatted id' 
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      status: 'error',
+      error: error.message
+    })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(unknownEndpoint) // impl. custom middleware
@@ -129,5 +128,5 @@ app.use(errorHandler) // impl. err. handler
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- 1_notes_backend/models/note.js
+++ 1_notes_backend/models/note.js
@@ -6,25 +6,25 @@ mongoose.set('strictQuery', false) // allow querying without strictly follow Sch
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => console.log('connected to MongoDB'))
-    .catch((error) => console.log('error connecting to MongoDB', error.message))
+  .then(_result => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB', error.message))
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    important: Boolean
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean
 })
 
 // modify the `toJSON` method of the schema (applied when retrieving data from MongoDB)
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 // if the first param is `Note` (singular), then Mongoose will automatically set the collection name to `notes` (plural)
