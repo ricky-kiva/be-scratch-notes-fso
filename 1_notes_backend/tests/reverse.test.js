1c18ab5414531d042287e66691ab58bc927d66a0
ricky-kiva <ricky.kiva@gmail.com>
2024-04-23 14:43:00 +0700

    refactor(test): separate test func. to corresponding files

--- 1_notes_backend/tests/reverse.test.js
+++ 1_notes_backend/tests/reverse.test.js
@@ -2,20 +2,11 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
 const reverse = require('../utils/for_testing').reverse
-const average = require('../utils/for_testing').average
 
 describe('reverse', () => {
   test('of "a"', () => assert.strictEqual(reverse('a'), 'a'))
   test('of "react"', () => assert.strictEqual(reverse('react'), 'tcaer'))
   test('of "saippuakauppias"', () =>
     assert.strictEqual(reverse('saippuakauppias'), 'saippuakauppias')
-  )
-})
-
-describe('average', () => {
-  test('of one value is the value itself', () => assert.strictEqual(average([1]), 1))
-  test('of empty array is zero', () => assert.strictEqual(average([]), 0))
-  test('of many is calculated right', () =>
-    assert.strictEqual(average([1, 2, 3, 4, 5, 6]), 3.5)
   )
 })
