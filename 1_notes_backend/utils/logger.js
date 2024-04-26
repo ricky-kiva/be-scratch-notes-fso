8dc7bf7ff8b900cd32e927e1b53d30da0eeb673e
ricky-kiva <ricky.kiva@gmail.com>
2024-04-26 19:07:27 +0700

    feat(utils): display `logger` only when not in test env.

--- 1_notes_backend/utils/logger.js
+++ 1_notes_backend/utils/logger.js
@@ -1,5 +1,9 @@
-const info = (...params) => console.log(...params)
+const info = (...params) => {
+  if (process.env.NODE_ENV !== 'test') { console.log(...params) }
+}
 
-const error = (...params) => console.log(...params)
+const error = (...params) => {
+  if (process.env.NODE_ENV !== 'test') { console.log(...params) }
+}
 
 module.exports = { info, error }
