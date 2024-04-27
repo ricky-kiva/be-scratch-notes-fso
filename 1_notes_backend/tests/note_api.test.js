168e943830f3d8dafb749c07257815de4c76dadb
ricky-kiva <ricky.kiva@gmail.com>
2024-04-27 19:34:08 +0700

    feat(test): add 2 `Note` API tests

--- 1_notes_backend/tests/note_api.test.js
+++ 1_notes_backend/tests/note_api.test.js
@@ -72,4 +72,33 @@ test('note without content won\'t be added', async () => {
   assert.strictEqual(notesAfter.length, helper.initialNotes.length)
 })
 
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultNote.body, noteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAfter = await helper.notesInDb()
+
+  const contents = notesAfter.map(r => r.content)
+  assert(!contents.includes(noteToDelete.content))
+
+  assert.strictEqual(notesAfter.length, helper.initialNotes.length - 1)
+})
+
 after(async () => { await mongoose.connection.close() })
