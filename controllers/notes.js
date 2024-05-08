bad10e84d14026bef3aad97fb06173b5d1f6b7b0
ricky-kiva <ricky.kiva@gmail.com>
2024-05-08 12:39:36 +0700

    feat(ctrl): impl. POST Note with User data

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,7 @@
 const notesRouter = require('express').Router()
+
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   // equivalent to:
@@ -20,12 +22,19 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
-    important: body.important || false
+    important: body.important || false,
+    user: user.id
   })
 
   const savedNote = await note.save()
+
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
