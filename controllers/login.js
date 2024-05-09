aa4b989233e15caf46c6471959d0f2b3f348505e
ricky-kiva <ricky.kiva@gmail.com>
2024-05-09 11:51:54 +0700

    feat(ctrl-login): impl. `JWT` expiry

--- controllers/login.js
+++ controllers/login.js
@@ -23,7 +23,11 @@ loginRouter.post('/', async (request, response) => {
     id: user._id
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(
+    userForToken,
+    process.env.SECRET,
+    { expiresIn: 60*60 } // 1 hour
+  )
 
   response.status(200).send({
     token,
